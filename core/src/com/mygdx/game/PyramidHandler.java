2f5336045c9cf419e2544cc00f3ad847b1fdde03
Pavlo Andriiash <96pasha@ukr.net>
2019-02-14 09:01:43 +0100

    new version totally
    working well but without interface and normal pyramid xDD

--- core/src/com/mygdx/game/PyramidHandler.java
+++ core/src/com/mygdx/game/PyramidHandler.java
@@ -1,75 +1,63 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
-
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class PyramidHandler extends InputAdapter implements ApplicationListener {
 
-	protected Label label;
-	protected BitmapFont font;
-	protected Stage stage;
-	protected long startTime;
-	public PerspectiveCamera cam;
-	final float[] startPos = {150f, -9f, 0f};
+	private AssetManager assets;
+	private Label label;
+	private BitmapFont font;
+	private Stage stage;
+	private PerspectiveCamera cam;
 
-	public Model model;
-	public ModelInstance instance;
+	private long startTime;
+	private float width = 200f, height = width / 2f, depth = width, bound = 45f;
+	private final float[] startPos = {150f, -9f, 0f}, Vpos = new float[3], pos = {startPos[0], startPos[1], startPos[2]};
+	private final float speed = 2f;
 
-	public ModelBatch modelBatch;
+	private Model model;
+	private ModelInstance instance;
+	private ModelBatch modelBatch;
+	private Environment environment;
 
-	public Environment environment;
+	private boolean loading;
 
-	public float width = 200f, height = width / 2f, depth = width;
-
-	Color clr;
-	Random rnd = new Random();
-
-
-	final float bound = 45f;
-	float[] pos = {startPos[0], startPos[1], startPos[2]};
-	float[] Vpos = new float[3];
-	final float speed = 2f;
+	private Color clr;
+	private Random rnd = new Random();
 
 	private float getSpeed() {
 		return speed * Math.signum((float) Math.random() - 0.5f) * Math.max((float) Math.random(), 0.5f);
 	}
 
+/*
 	public void setColor(Color newColor)
 	{
 		model = new ModelBuilder().createCone(width, height, depth, 3,
 				new Material(ColorAttribute.createDiffuse(newColor)),
 				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
 		instance = new ModelInstance(model);
-		//instance.transform.setToRotation(Vector3.Z, 90).translate(-5,0,0);
-
+		instance.transform.setToRotation(Vector3.Z, 90).translate(-5,0,0);
 	}
+*/
 
 	@Override
 	public void resize(int width, int height) {
@@ -91,14 +79,11 @@ public class PyramidHandler extends InputAdapter implements ApplicationListener 
 		cam.far = 300f;
 		cam.update();
 
-		setColor(Color.GREEN);
+		//setColor(Color.GREEN);
 
-
-		// initialize speed
 		for (int i = 0; i < 3; i++){
 			Vpos[i] = getSpeed();
 		}
-
 
 		font = new BitmapFont();
 		label = new Label(" ", new Label.LabelStyle(font, Color.WHITE));
@@ -109,10 +94,22 @@ public class PyramidHandler extends InputAdapter implements ApplicationListener 
 		startTime = System.currentTimeMillis();
 
 		Gdx.input.setInputProcessor(new InputMultiplexer(this));
+
+		assets = new AssetManager();
+		assets.load("1.g3db", Model.class);
+		loading = true;
 	}
 
 	@Override
 	public void render() {
+		if (loading)
+			if (assets.update()) {
+				model = assets.get("1.g3db", Model.class);
+				instance = new ModelInstance(model);
+				loading = false;
+			} else {
+				return;
+			}
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
@@ -126,7 +123,6 @@ public class PyramidHandler extends InputAdapter implements ApplicationListener 
 		builder.append("| Game time: ").append(time);
 		label.setText(builder);
 		stage.draw();
-
 	}
 
 	@Override
@@ -144,7 +140,7 @@ public class PyramidHandler extends InputAdapter implements ApplicationListener 
 
 	@Override
 	public void resume() {}
-
+/*
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		clr = new Color(0xFF000000 | rnd.nextInt(0xFFFFFF));
@@ -154,9 +150,9 @@ public class PyramidHandler extends InputAdapter implements ApplicationListener 
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-
 		return true;
 	}
+*/
 
 	public void shake()
 	{
